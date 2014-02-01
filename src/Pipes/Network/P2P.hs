b6f1130a48ae9f11c98e0bde80f011aab83965d4
Danny Navarro <j@dannynavarro.net>
2014-02-01 20:27:15 +0100

    Initial package scaffolding

---
+++ src/Pipes/Network/P2P.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+module Pipes.Network.P2P where
+
+import Control.Error
+import Control.Applicative (pure)
+import Control.Monad ((>=>), void, guard)
+-- import Data.Monoid (mappend)
+import Control.Concurrent (ThreadId, myThreadId, forkIO)
+import Control.Concurrent.MVar
+import GHC.Generics (Generic)
+import Data.Binary (Binary(put,get))
+import qualified Data.Binary as Binary
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.ByteString.Lazy (toStrict, fromStrict)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Pipes
+import Pipes.Lift (errorP)
+import qualified Pipes.Prelude as P
+import Pipes.Binary (decodeMany)
+import Pipes.Concurrent (Output, Input, Buffer(Unbounded), toOutput)
+import Pipes.Network.TCP
+  ( HostPreference
+  , HostName
+  , ServiceName
+  , fromSocket
+  , connect
+  , send
+  , serve
+  , recv
+  , fromSocket
+  , Socket
+  , SockAddr
+  )
+
+instance Binary HostPreference where
+    put = undefined
+    get = undefined
+
+type Mailbox = (Output Message, Input Message)
+
+data Node = Node
+    { settings    :: Settings
+    , connections :: MVar (Map ThreadId Connection)
+    , broadcaster :: MVar Mailbox
+    }
+
+data Settings = Settings
+    { preference :: HostPreference
+    , service    :: ServiceName
+    } deriving (Show, Eq, Generic)
+
+instance Binary Settings
+
+data Connection = Connection
+    { sock    :: Socket
+    , sockAdd :: SockAddr
+    }
+
+instance Binary SockAddr where
+    put = undefined
+    get = undefined
+
+data Message = GETADDR
+             | ADDR SockAddr
+             | VER Int
+             | ACK
+               deriving (Show, Eq, Generic)
+
+instance Binary Message
+
+type Seed = (HostName, ServiceName)
+
+-- | Launch a node.
+launch :: Node -> Seed -> IO ()
+launch node@Node{..} (hn, sn) = do
+    serve (preference settings) (service settings) undefined -- (handle node . fst)
+    void . forkIO . connect hn sn $ \(sock, sockAddr) -> void . runMaybeT $ do
+        -- handshake
+        lift $ send sock (encode $ VER version)
+        let liftMay = lift >=> hoistMaybe
+        ver <- liftMay $ recv sock verLen
+        ack <- liftMay $ recv sock ackLen
+        guard $ decode ver <= version
+        guard $ decode ack == ACK
+
+        -- Request addresses, register and handle incoming messages
+        lift $ do send sock $ encode GETADDR
+                  -- mb@(output, _) <- spawn Unbounded
+                  -- modifyMVar_ broadcaster $ pure . first (mappend output)
+                  tid <- myThreadId
+                  let conn = Connection sock sockAddr
+                  modifyMVar_ connections $ pure . Map.insert tid conn
+                  handle node conn
+
+handle :: Node -> Connection -> IO ()
+handle Node{..} Connection{..} = undefined
+  where
+    go = do (output, input) <- readMVar broadcaster
+            let x = errorP (decodeMany (fromSocket sock 4096)) >-> P.map snd >-> toOutput output
+            return ()
+
+ackLen :: Int
+ackLen = B.length $ encode ACK
+
+verLen :: Int
+verLen = B.length $ encode version
+
+version :: Int
+version = 2
+
+encode :: Binary a => a -> ByteString
+encode = toStrict . Binary.encode
+
+decode :: Binary a => ByteString -> a
+decode = Binary.decode . fromStrict
