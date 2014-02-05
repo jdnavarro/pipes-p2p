0eb42b067c6c32e0f249940cd098da908d57e5ed
Danny Navarro <j@dannynavarro.net>
2014-02-05 11:22:40 +0100

    Multiple changes
    - Simplify `Connection`
    - Substitute `SockAddr` by `Address` (hostname and service).
    - Distinguish between incoming (`connectI`) and outgoing (`connectO`)
      connection.
    - Handle incoming messages.

--- src/Pipes/Network/P2P.hs
+++ src/Pipes/Network/P2P.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
@@ -6,20 +7,29 @@ module Pipes.Network.P2P where
 import Control.Error
 import Control.Applicative ((<$>), (<*>), pure)
 import Control.Monad ((>=>), void, guard)
--- import Data.Monoid (mappend)
-import Control.Concurrent (ThreadId, myThreadId, forkIO)
+import Control.Concurrent (forkIO)
 import Control.Concurrent.MVar
 import GHC.Generics (Generic)
-import qualified Data.Binary as Binary
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Data.ByteString.Lazy (toStrict, fromStrict)
+import qualified Data.Binary as Binary(encode,decode)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Lens.Family2 ((^.))
 import Pipes
-import Pipes.Lift (errorP)
-import Pipes.Binary (Binary(put,get), decoded)
-import Pipes.Concurrent (Output, Input, Buffer(Unbounded), spawn, toOutput)
+import qualified Pipes.Prelude as P
+import Control.Monad.Trans.Error
+import Pipes.Lift (errorP, runErrorP)
+import Pipes.Binary (Binary, decoded, DecodingError)
+import Pipes.Concurrent
+  ( Buffer(Unbounded)
+  , Output
+  , Input
+  , spawn
+  , toOutput
+  , fromInput
+  )
 import Pipes.Network.TCP
   ( HostPreference
   , HostName
@@ -30,83 +40,87 @@ import Pipes.Network.TCP
   , serve
   , recv
   , fromSocket
+  , toSocket
   , Socket
-  , SockAddr
   )
-import Lens.Family2 ((^.))
-
-instance Binary HostPreference where
-    put = undefined
-    get = undefined
 
 type Mailbox = (Output Message, Input Message)
 
 data Node = Node
     { settings    :: Settings
-    , connections :: MVar (Map ThreadId Connection)
+    , connections :: MVar (Map Address Socket)
     , broadcaster :: MVar Mailbox
     }
 
 node :: Settings -> IO Node
-node settings = Node settings <$> conns <*> mb
-  where
-    conns = newMVar Map.empty
-    mb    = spawn Unbounded >>= newMVar
+node settings = Node settings
+            <$> newMVar Map.empty
+            <*> (newMVar =<< spawn Unbounded)
 
 data Settings = Settings
-    { preference :: HostPreference
-    , service    :: ServiceName
+    { hostPreference :: HostPreference
+    , serviceName    :: ServiceName
     } deriving (Show, Eq, Generic)
 
-instance Binary Settings
+data Address = Address HostName ServiceName
+               deriving (Show, Eq, Ord, Generic)
 
-data Connection = Connection
-    { sock    :: Socket
-    , sockAdd :: SockAddr
-    }
-
-instance Binary SockAddr where
-    put = undefined
-    get = undefined
+instance Binary Address
 
 data Message = GETADDR
-             | ADDR SockAddr
+             | ADDR Address
              | VER Int
              | ACK
+             | RELAY Address
                deriving (Show, Eq, Generic)
 
 instance Binary Message
 
-type Seed = (HostName, ServiceName)
+launch :: Node -> Address -> IO ()
+launch n@Node{..} addr = connectO n addr >>
+    serve (hostPreference settings)
+          (serviceName settings)
+          (connectI n . fst)
 
--- | Launch a node.
-launch :: Node -> Seed -> IO ()
-launch n@Node{..} (hn, sn) = do
-    serve (preference settings) (service settings) undefined -- (handle n . fst)
-    void . forkIO . connect hn sn $ \(sock, sockAddr) -> void . runMaybeT $ do
+connectO :: Node -> Address -> IO ()
+connectO n@Node{..} addr@(Address hn sn) =
+    void . forkIO . connect hn sn $ \(sock, _) -> void . runMaybeT $ do
         -- handshake
         lift $ send sock (encode $ VER version)
-        let liftMay = lift >=> hoistMaybe
-        ver <- liftMay $ recv sock verLen
-        ack <- liftMay $ recv sock ackLen
+        let liftMaybe = lift >=> hoistMaybe
+        ver <- liftMaybe $ recv sock verLen
+        ack <- liftMaybe $ recv sock ackLen
         guard $ decode ver <= version
         guard $ decode ack == ACK
-
         -- Request addresses, register and handle incoming messages
         lift $ do send sock $ encode GETADDR
-                  -- mb@(output, _) <- spawn Unbounded
-                  -- modifyMVar_ broadcaster $ pure . first (mappend output)
-                  tid <- myThreadId
-                  let conn = Connection sock sockAddr
-                  modifyMVar_ connections $ pure . Map.insert tid conn
-                  handle n conn
+                  modifyMVar_ connections $ pure . Map.insert addr sock
+                  handle n sock
 
-handle :: Node -> Connection -> IO ()
-handle Node{..} Connection{..} = go
-  where
-    go = do (output, input) <- readMVar broadcaster
-            let p = errorP (fromSocket sock 4096 ^. decoded)  >-> toOutput output
-            return ()
+connectI :: Node -> Socket -> IO ()
+connectI = undefined
+
+-- TODO: How to get rid of this?
+instance Error (DecodingError, Producer ByteString IO ())
+
+handle :: Node -> Socket -> IO ()
+handle n@Node{..} sock = void . forkIO $ do
+    (_, inbc) <- readMVar broadcaster
+    (outr, inr) <- spawn Unbounded
+    void . forkIO . runEffect . void . runErrorP
+         $ errorP (fromSocket sock 4096 ^. decoded) >-> toOutput outr
+    void . forkIO . runEffect $ fromInput inbc >-> toOutput outr
+    runEffect $ fromInput inr >-> do
+        msg <- await
+        case msg of
+             ACK        -> return ()
+             GETADDR    -> do conns <- liftIO $ readMVar connections
+                              each (Map.keys conns) >-> P.map encode >-> toSocket sock
+             ADDR addr  -> do conns <- liftIO $ readMVar connections
+                              if Map.member addr conns
+                              then return ()
+                              else liftIO $ connectO n addr
+             RELAY addr -> send sock (encode addr) -- TODO: Guard against same connection relay
 
 ackLen :: Int
 ackLen = B.length $ encode ACK
