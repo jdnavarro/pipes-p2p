00160a073d8b64099ff8ba83a2ca77e4102aa61b
Danny Navarro <j@dannynavarro.net>
2014-02-06 16:46:07 +0100

    Implement `SockAddr` `Binary` instance

--- src/Pipes/Network/Internal.hs
+++ src/Pipes/Network/Internal.hs
@@ -1,14 +1,28 @@
+{-# LANGUAGE LambdaCase #-}
 module Pipes.Network.Internal where
 
+import Control.Applicative ((<$>), (<*>), (*>))
 import Data.ByteString (ByteString)
 import Data.ByteString.Lazy (toStrict, fromStrict)
-import Network.Socket (SockAddr, Socket)
-import Data.Binary (Binary(put,get))
+import Data.Binary (Binary(put,get), putWord8, getWord8)
 import qualified Data.Binary as Binary(encode,decode)
+import Network.Socket
+  ( SockAddr(SockAddrInet, SockAddrInet6, SockAddrUnix)
+  , PortNumber(PortNum)
+  , Socket
+  )
 
 instance Binary SockAddr where
-    put = undefined
-    get = undefined
+    put (SockAddrInet (PortNum port) host) =
+        putWord8 0 *> put (port, host)
+    put (SockAddrInet6 (PortNum port) flow host scope) =
+        putWord8 1 *> put (port, flow, host, scope)
+    put (SockAddrUnix str) = putWord8 2 *> put str
+
+    get = getWord8 >>= \case
+              0 -> SockAddrInet <$> PortNum <$> get <*> get
+              1 -> SockAddrInet6 <$> PortNum <$> get <*> get <*> get <*> get
+              2 -> SockAddrUnix <$> get
 
 newSocket :: SockAddr -> IO Socket
 newSocket = undefined
@@ -21,4 +35,3 @@ encode = toStrict . Binary.encode
 
 decode :: Binary a => ByteString -> a
 decode = Binary.decode . fromStrict
-
