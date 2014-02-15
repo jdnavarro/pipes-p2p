596340cd2b25dea36ceb80d4bed3ad580772e82a
Danny Navarro <j@dannynavarro.net>
2014-02-15 16:52:12 +0100

    Tweak demo program to make it easier to debug

--- examples/demo.hs
+++ examples/demo.hs
@@ -18,12 +18,19 @@ main = flip finally (traverse_ removeFile [path1,path2]) $ do
         addr3 = SockAddrInet 1234 iNADDR_ANY
         addr4 = SockAddrInet 1235 iNADDR_ANY
         magic = 2741
+    putStrLn "Node1:"
     n1 <- node Nothing magic addr1
-    n2 <- node Nothing magic addr2
-    n3 <- node Nothing magic addr3
-    n4 <- node Nothing magic addr4
     _ <- forkIO $ launch n1 []
+    threadDelay $ 10 ^ (6::Int)
+    putStrLn "Node2:"
+    n2 <- node Nothing magic addr2
     _ <- forkIO $ launch n2 [addr1]
+    threadDelay $ 10 ^ (6::Int)
+    putStrLn "Node3:"
+    n3 <- node Nothing magic addr3
     _ <- forkIO $ launch n3 [addr1,addr2]
+    threadDelay $ 10 ^ (6::Int)
+    putStrLn "Node4:"
+    n4 <- node Nothing magic addr4
     _ <- forkIO $ launch n4 [addr3]
     threadDelay $ 100 ^ (6::Int)
