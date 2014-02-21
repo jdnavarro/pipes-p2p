c268215b6be76afc90bfd8f300a42b78310a0fff
Danny Navarro <j@dannynavarro.net>
2014-02-21 11:18:53 +0100

    Remove unnecessary pragma

--- src/Pipes/Network/P2P.hs
+++ src/Pipes/Network/P2P.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
